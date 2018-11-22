32a849d8dbc20d9c9cced99dfe048392010b4acd
simplyeleza <techdocpcu@gmail.com>
2018-11-22 16:36:43 +0900

    using destroy to delete an entry
    destroy expects only one parameter.
    We are not getting anything back so we remove the response object.

--- server.js
+++ server.js
@@ -76,6 +76,21 @@ app.get('/findOne',(req,res)=>{
 })
 
 
+app.delete('/remove',(req,res)=>{
+    User.destroy({
+       where: { id: 50} 
+    })
+    .then(() =>{
+       res.send('User successfully deleted');
+   })
+   .catch(error =>{
+       console.log(error);
+       res.status(404).send(error);
+   })
+   
+   })
+
+
 app.put('/update',(req,res)=>{
     User.update({
         name:'Michal Power',
